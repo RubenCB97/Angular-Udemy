bf21c9961c4228f4bcbed61e482c5e00e67183d5
Rubén <rubencostab97@gmail.com>
2022-10-12 22:28:23 +0200

    v4

---
+++ 04-pipesApp/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+
+//Cambiar el locale de la app
+import localEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localEs);
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es'} //Idioma en español
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
